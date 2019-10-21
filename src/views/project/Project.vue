9b75c05707915eed3e3c4c5cf4790ab699e7de34
WaterMargin <43610689+wangshuihu@users.noreply.github.com>
2019-10-21 18:12:37 +0800

    Update Project.vue

--- src/views/project/Project.vue
+++ src/views/project/Project.vue
@@ -64,7 +64,7 @@
                           <template slot="title">
                             <span>本地运行更清晰</span>
                           </template>
-                          <a-button type="primary" @click="openArc()">本地运行</a-button>
+                          <a-button type="primary" @click="openArc()" :disabled="disabled">下载</a-button>
                         </a-tooltip>
                       </div>
                     </div>
@@ -98,7 +98,7 @@
                           <template slot="title">
                             <span>本地运行更清晰</span>
                           </template>
-                          <a-button type="primary" @click="openPip()">本地运行</a-button>
+                          <a-button type="primary" @click="openPip()" :disabled="disabled">下载</a-button>
                         </a-tooltip>
                       </div>
                     </div>
@@ -159,6 +159,8 @@ export default {
       companyId: "",
       projectName: "",
       visible: false,
+      disabled:false,
+      size: 'large',
     };
   },
   mounted(){
@@ -214,75 +216,75 @@ export default {
       let mythis=this;
       let projectId = this.$route.query.projectId;
       this.$http.get(api.OpenApp+"?projectId="+projectId).then(function (response) {
-          if (response.data.visitUrl == null || response.data.scheme==null){
-              alert("改模型没有本地程序")
+        console.log(response)
+          if (response.data == null){
+         mythis.openNotification();
           }
-        var url=response.data.visitUrl;
-        var scheme=response.data.scheme;
-        //判断浏览器
-        var u = navigator.userAgent;
-        // if(/MicroMessenger/gi.test(u){
-        //   // 引导用户在浏览器中打开
-        //   alert('请在浏览器中打开');
-        //   return;
-        // };
-        var d = new Date();
-        var t0 = d.getTime();
-        if(u.indexOf('Android') > -1 || u.indexOf('Linux') > -1){
-          //Android
-
-          if( mythis.openApp(scheme)){
-            mythis.openApp(scheme)
-          }else{
-            //由于打开需要1～2秒，利用这个时间差来处理－－打开app后，返回h5页面会出现页面变成app下载页面，影响用户体验
-            var delay = setInterval(function(){
-              var d = new Date();
-              var t1 = d.getTime();
-              if( t1-t0<3000 && t1-t0>2000){
-                alert('请下载APP');
-                window.location.href = url;
-              }
-              if(t1-t0>=3000){
-                clearInterval(delay);
-              }
-            },1000);
-          }
-        }else if(u.indexOf('iPhone') > -1){
-          //IOS
-          if(mythis.openApp(scheme)){
-            mythis.openApp(scheme);
-          }else{
-            var delay = setInterval(function(){
-              var d = new Date();
-              var t1 = d.getTime();
-              if( t1-t0<3000 && t1-t0>2000){
-                alert('请下载APP');
-                window.location.href = url;
-              }
-              if(t1-t0>=3000){
-                clearInterval(delay);
-              }
-            },1000);
-          }
+        if (response.data.type==1) {
+          window.location.href = response.data.visitUrl;
+          mythis.disabled=true;
+          var type = response.data.type;
+          mythis.appDownload(projectId,type);
+        }else {
+          mythis.openNotification();
         }
+
       }).catch(function (error) {
         console.log(error);
-          alert("改模型没有本地程序")
+          alert("该模型没有本地程序")
       })
     },
 
-     openApp(scheme) {
-// 通过iframe的方式试图打开APP，如果能正常打开，会直接切换到APP，并自动阻止a标签的默认行为
-// 否则打开a标签的href链接
-    var ifr = document.createElement('iframe');
-    ifr.src = scheme;
-    ifr.style.display = 'none';
-    document.body.appendChild(ifr);
-    window.setTimeout(function(){
-      document.body.removeChild(ifr);
-    },2000);
-  },
+    appDownload(projectId,type){
+      this.$http.get(api.AppAdd+"?projectId="+projectId+"&type="+type).then(function (res) {
+        console.log(res);
+      }).catch(function (error) {
 
+      })
+    },
+    openPip(){
+      let mythis=this;
+      let projectId = this.$route.query.projectId;
+      this.$http.get(api.OpenApp+"?projectId="+projectId).then(function (response) {
+        console.log(response)
+        if (response.data == null){
+          mythis.openNotification();
+        }
+        if (response.data.type==2) {
+          window.location.href = response.data.visitUrl;
+          mythis.disabled=true;
+          var type = response.data.type;
+          mythis.appDownload(projectId,type);
+        }else {
+          mythis.openNotification();
+        }
+
+      }).catch(function (error) {
+        console.log(error);
+        alert("该模型没有本地程序")
+      })
+    },
+//      openApp(scheme) {
+// // 通过iframe的方式试图打开APP，如果能正常打开，会直接切换到APP，并自动阻止a标签的默认行为
+// // 否则打开a标签的href链接
+//     var ifr = document.createElement('iframe');
+//     ifr.src = scheme;
+//     ifr.style.display = 'none';
+//     document.body.appendChild(ifr);
+//     window.setTimeout(function(){
+//       document.body.removeChild(ifr);
+//     },2000);
+//   },
+    openNotification() {
+      this.$notification.open({
+        message: '提示',
+        description:
+                '该模型没有本地程序,等待管理员上传！',
+        onClick: () => {
+          console.log('Notification Clicked!');
+        },
+      });
+    },
     showDrawer() {
       this.visible = true;
     },
