474f47d8cba4686f85611200b06717752a11c14d
WaterMargin <43610689+wangshuihu@users.noreply.github.com>
2019-10-21 18:10:11 +0800

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import global from "@/api/global";
 
 const version = global.url + "/v" + global.version;
-const checkVersion = global.url+"/";//打包
+const checkVersion = global.url;//版本检查
 const api = {
   Login: version + "/user/login",
   GetProject: version + "/userProject/getProject",
@@ -9,9 +9,11 @@ const api = {
   GetResModel: version + "/userProject/getResModel",
   GetResDrawing: version + "/userProject/getResDrawing",
   GetResMeterial: version + "/userProject/getResMeterial",
-  CheckVersion: checkVersion+ "/version/checkVersion",
+  CheckVersion: checkVersion+ "/version/checkVersion",//版本检查
   Pay: version + "/userProject/pay",//支付宝支付
   OpenApp:version+"/app/openApp",//打开app
   AllApp:version+"/app/allApp",//所有下载app
+  AppAdd:version+"/app/appAdd",//添加下载记录
+  SelectApp:version+"/app/selectApp",//查询用户所下载app
 };
 export default api;
